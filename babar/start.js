7421f768dc602d315bbb57696dc6f0f30dfd96d4
Jackson Willis <railsmail42@gmail.com>
2011-12-19 09:45:30 -0600

    Added example branch to show off features

--- babar/start.js
+++ babar/start.js
@@ -1,3 +1,4 @@
 load('http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js')
 .then('https://raw.github.com/cowboy/jquery-hashchange/v1.3/jquery.ba-hashchange.min.js')
-.then('babar/babar.js').then('app/config.babar.js');
+.then('https://raw.github.com/jacksonwillis/babar.js/master/babar/babar.min.js')
+.then('app/config.babar.js');
